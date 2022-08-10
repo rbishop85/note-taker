c1048f9e40755ed7ba0745f5062802314265c5a9
Robert Bishop <rbishop85@gmail.com>
2022-08-09 19:47:38 -0700

    started work on app.post api route

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.get('*', (req, res) =>
 );
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post('/api/notes', (req, res) => {
+
+  const { title, text } = req.body;
+
+});
     // accepting new note
     // pushing note to notes list
     // using notes list to update db.json file (FS module fs.writeFile (rewrite the entire file each time))
