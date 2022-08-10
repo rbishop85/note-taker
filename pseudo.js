b168d7b0eeb04a43dd6c10d65c41c0c71f83a9f8
Robert Bishop <rbishop85@gmail.com>
2022-08-09 19:40:26 -0700

    setup api route for retrieving notes

--- pseudo.js
+++ pseudo.js
@@ -1,32 +1,29 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+// Require express
 
-const notes = require('./db/db.json');
+// Require the db.json file and store it in 'notes'
 
-const app = express();
 
-const PORT = 3001;
+// Use express to intialize the 'app' server
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// const PORT will need to be assigned to Heroku's dynamic port
 
-app.use(express.static('public'));
+
+// Have the 'app' use appropriate middleware to parse body data
+// middle ware sample from activity 15, app.use(express.json()) and express.urlencoded
+
 
 // 4 routes, maybe work on sorting the html routes and the api routes sorted into separate JS files (check pre-class from day 2)
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+// GET /notes should return the notes.html file.
 
-app.get('/api/notes', (req, res) => {
-  return res.json(notes);
-});
+
+
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+    // res.json()
 
 // ?? Should this be under the /notes app.get above, or stay down here?
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+// GET * should return the index.html file.
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
     // accepting new note
@@ -36,6 +33,4 @@ app.get('*', (req, res) =>
 
 // Optional delete function will just delete the item from the array and then rewrite the json file?
 
-app.listen(PORT, () =>
-  console.log(`Note taking app server running on port: ${PORT}!`)
-);
+// Use the 'app' to 'listen' to a PORT
