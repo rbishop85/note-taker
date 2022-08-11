f624f10dd7d7dc16324153c7aa0080402fdee374
Robert Bishop <rbishop85@gmail.com>
2022-08-10 17:53:16 -0700

    changed port const to be setup to work with Heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const notes = require('./db/db.json');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
